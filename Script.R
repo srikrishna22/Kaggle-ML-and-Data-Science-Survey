b9e2e1ae180ac75a03ee15cad45b7a2f487b54c0
Srikrishna Venkat <srikrishnav22@gmail.com>
2018-03-26 12:52:30 -0500

    added more code

--- Script.R
+++ Script.R
@@ -11,8 +11,32 @@ library(broom)
 responses <- read_csv("./multipleChoiceResponses.csv")
 
 
-#subset of the responses
+#subset of the responses selected and  str_split() to get individual tools that were locked in 
+#strings. This will act as main data frame for rest of analysis.
+
 sr <- responses %>% select(WorkToolsSelect, LanguageRecommendationSelect, EmployerIndustry,
                       WorkAlgorithmsSelect) %>%
                      mutate(WorkToolsSelect = str_split(WorkToolsSelect, pattern = ',')) %>%
                       unnest()
+
+# arrange the tools that were used most often which tools were used
+
+tools_used <- sr %>%
+                group_by(tool = WorkToolsSelect) %>%
+                summarize(count = n()) %>%
+                arrange(desc(count))
+
+#bar chart
+
+ggplot(tools_used, aes(x = tool, y = count)) +
+  geom_bar(stat = 'identity') +
+  coord_flip()
+  
+# R vs Python
+
+
+
+
+
+
+
